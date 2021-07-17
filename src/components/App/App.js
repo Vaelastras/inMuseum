0b26faa5e1b61cc037f50f065d4fbd6a1cdcb2e1
Alexey Belov <vaelastras@gmail.com>
2021-07-17 16:17:11 +0300

    added description

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 import Header from "../Header/Header";
 import About from "../About/About";
 import Footer from "../Footer/Footer";
+import Description from "../Description/Description";
 
 function App() {
   return(
     <>
       <Header/>
       <About/>
+      <Description/>
       <Footer/>
     </>
   )
